58c6f0e87555bbaacebfbad92f2157cac4d57a43
ankitkumargupta <ankitgupta6881@gmail.com>
2025-01-12 19:32:04 +0530

    form handling using controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <form className='w-60 h-52 items-center p-4 border border-zinc-400' action="" onSubmit={handleSubmit}>
+      <form className='w-60 h-52 items-center p-4 border border-zinc-500' action="" onSubmit={handleSubmit}>
         <input onChange={event=>setVal({...val, name: event.target.value})} className='m-2 border border-zinc-400' type="text" placeholder='Name' />
         <input onChange={event=>setVal({...val, email: event.target.value})} className='m-2 border border-zinc-400' type="email" placeholder='email' />
         <div>
